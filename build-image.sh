16a91fa59d9a7237225125d8b78ecde7333b7ba8
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2025-01-26 10:58:19 -0500

    Bump version to 0.3

--- build-image.sh
+++ build-image.sh
@@ -2,8 +2,8 @@
 
 docker build -t cgit:temp ./docker
 
-read -p "Enter version [0.2]: " version
-version=${version:-0.2}
+read -p "Enter version [0.3]: " version
+version=${version:-0.3}
 
 slim build --continue-after 1 --http-probe=false --include-path-file=./docker/keep-paths-list --target cgit:temp --tag truebad0ur/cgit:"$version"-`arch`
 
